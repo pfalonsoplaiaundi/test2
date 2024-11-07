4662f34bf7bda9050bcddd0100b75dc1579fc4ca
Pablo.FA <ikdhb@plaiaundi.net>
2024-11-07 10:01:41 +0100

    a

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.print("Puto");
+        int a = 1;
+        System.out.println(a);
     }
 }
